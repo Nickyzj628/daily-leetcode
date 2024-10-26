c001738d7e34309a32492d3d28f25604192c8b1e
Nickyzj628 <1927139831@qq.com>
2024-10-26 10:20:20 +0800

    perf: 优化斐波那契数列

--- essentials/fibonacci.js
+++ essentials/fibonacci.js
@@ -1,13 +1,26 @@
 /**
  * 斐波那契数列
+ *
+ * F(0) = 0
+ *
+ * F(1) = 1
+ *
+ * F(n) = F(n-1) + F(n-2)（对于 n ≥ 2）
+ *
  * @param {number} n
  */
 function fibonacci(n) {
-    const dp = [0, 1];
+    if (n < 2) return n;
+    // 从空间上把Array(n)优化到n-1和n-2两个变量
+    let pre = 0;
+    let pre2 = 1;
+    // 自底向上计算，从时间上去除了重复计算
     for (let i = 2; i <= n; i++) {
-        dp[i] = dp[i - 1] + dp[i - 2];
+        let cur = pre + pre2;
+        pre2 = pre;
+        pre = cur;
     }
-    return dp[n];
+    return pre;
 }
 
-console.log(fibonacci(10));
+console.log(fibonacci(10)); // 34
